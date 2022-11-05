c8fbb9b97d8efec148fd23910c0ab078f7c1e164
Marcel <marcelgast240@hotmail.com>
2022-11-05 13:06:38 +0100

    isPalindrome Solution

---
+++ PalindromeNumber.java
@@ -0,0 +1,33 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+
+/*
+https://leetcode.com/problems/palindrome-number/
+Given an integer x, return true if x is a palindrome, and false otherwise.
+ */
+class Solution {
+    public static boolean isPalindrome(int x) {
+        int tmp = x, cache = 0;
+        while(tmp > 0){
+            cache = cache*10 + tmp % 10;
+            tmp /= 10;
+        }
+        return x >= 0 && cache == x;
+    }
+}
+
+
+class SolutionTest {
+
+@Test
+public void testIsPalindrome () {
+    assertTrue(Solution.isPalindrome(121));
+    assertFalse(Solution.isPalindrome(-121));
+    assertFalse(Solution.isPalindrome(10));
+
+
+}
+}
