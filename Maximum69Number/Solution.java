44982838480ccc9a2db88ff5fea0436e2a0771b9
Marcel <marcelgast240@hotmail.com>
2022-11-13 13:29:17 +0100

    Changed Comment

--- Maximum69Number/Solution.java
+++ Maximum69Number/Solution.java
@@ -4,9 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 /*
-https://leetcode.com/problems/maximum-69-number/
-You are given a positive integer num consisting only of digits 6 and 9.
-Return the maximum number you can get by changing at most one digit (6 becomes 9, and 9 becomes 6).
+https://leetcode.com/problems/longest-common-prefix/
  */
 public class Solution {
     public static int maximum69Number(int num) {
